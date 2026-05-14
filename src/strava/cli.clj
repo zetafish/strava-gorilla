e039ec5f6a967886a859cbe591e5b8e3ed2b2a78
Endymion Kasanardjo <zetafish@gmail.com>
2026-05-14 16:48:30 +0200

    update

--- src/strava/cli.clj
+++ src/strava/cli.clj
@@ -1,7 +1,9 @@
 (ns strava.cli
-  (:require [strava.cli.eff :as eff]
+  (:require [babashka.cli :as cli]
+            [strava.cli.eff :as eff]
             [strava.cli.search :as search]
-            [strava.cli.sync :as sync]))
+            [strava.cli.sync :as sync]
+            [strava.repo :as repo]))
 
 (defn eff [args]
   (eff/run args))
@@ -11,3 +13,8 @@
 
 (defn search [args]
   (search/run args))
+
+(defn fetch-desc [args]
+  (let [opts (cli/parse-opts args {:spec {:id {:coerce :long :require true}}})]
+    (repo/get-description-by-activity-id (:id opts))
+    (println "Saved description for activity" (:id opts))))
--- src/strava/user.clj
+++ src/strava/user.clj
@@ -4,7 +4,23 @@
             [clojure.java.io :as io]
             [cognitect.transit :as transit]
             [strava.api :as api]
+            [strava.cli.sync :as sync]
             [strava.repo :as repo]
             [strava.track :as track]
             [strava.track.gpx :as gpx]
             [strava.track.tcx :as tcx]))
+
+(doseq [y [;; 2026
+           2025
+           2024
+           2023
+           2022
+           2021
+           2020
+           2019
+           2018
+           2017
+           2016
+           2015
+           2014]]
+  (sync/sync-year y))
